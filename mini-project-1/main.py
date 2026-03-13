e4581deb60c8533d1325d3fdf2bcccc35a116fef
polina <polina.zimina@final.edu.tr>
2026-03-14 00:11:30 +0300

    mini-project-1: add POST, PUT, DELETE endpoints

--- mini-project-1/main.py
+++ mini-project-1/main.py
@@ -19,5 +19,31 @@ async def read_patient_information_by_id(patient_id: int):
             return patient
     raise HTTPException(status_code = 404, detail = "Patient not found")
 
+@app.post("/patients/", response_model=Patient)
+async def add_patient(patient: Patient):
+    patients.append(patient)
+    return patient 
+
+@app.put("/patients/{patient_id}", response_model=Patient)
+async def update_patient(patient_id: int, updated_patient: Patient):
+    for idx, patient in enumerate(patients):
+        if patient.id == patient_id:
+            patients[idx] = updated_patient
+    raise HTTPException(status_code = 404, detail = "Patient not found")
+        
+
+
+@app.delete("/patients/{patient_id}")
+async def remove_patient(patient_id: int):
+    await asyncio.sleep(3)
+    for idx, patient in enumerate(patients):
+        if patient.id == patient_id:
+            patients.pop(idx)
+            return {"message": "Patient deleted"}
+    raise HTTPException(status_code = 404, detail = "Patient not found")
+
+ 
+
+
 
 
