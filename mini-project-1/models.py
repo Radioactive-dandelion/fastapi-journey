ad5b734dfdcf6693457801cc391d57727b1dff66
polina <polina.zimina@final.edu.tr>
2026-03-14 00:24:43 +0300

    mini-project-1: define Pydantic models with validation and made small changes

--- mini-project-1/models.py
+++ mini-project-1/models.py
@@ -16,18 +16,18 @@ class Appointment(BaseModel):
     status: Appointment_Status
     diagnosis: Optional[str] = None
 
-@validator("date")
-def check_date(cls, v):
-    from datetime import date as dt_date
-    if v < dt_date.today():
-        raise ValueError("Appoinment date cannot be in the past")
-    return v
+    @validator("date")
+    def check_date(cls, v):
+        from datetime import date as dt_date
+        if v < dt_date.today():
+            raise ValueError("Appoinment date cannot be in the past")
+        return v
 
 class Patient(BaseModel):
     id: int
     name: str = Field(..., min_length = 2, max_length = 50)
     age: int = Field(..., ge = 0, le = 120)
-    appointments: List[Appointment] = []
+    appointments: List[Appointment] = Field(default_factory=list)
 
 
 
