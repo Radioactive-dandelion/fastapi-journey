3c16e503a4dbfb78d60c3978ab3d58c14f94dbaa
polina <polina.zimina@final.edu.tr>
2026-03-28 21:26:05 +0300

    mini-project-2: make small changes in connection.py

--- mini-project-2/database/connection.py
+++ mini-project-2/database/connection.py
@@ -1,7 +1,7 @@
 import os
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from typing import Type, List
 from beanie import Document
 
